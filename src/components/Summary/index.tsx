2d606a81b1e57438897afd3579dc81d14d6f8d0e
Matheus <vicde2@hotmail.com>
2022-12-26 14:53:56 -0300

    DT-Money

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,35 @@
+import { SummaryContainer, SummaryCard } from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react';
+
+export function Summary(){
+    return(
+        <SummaryContainer>
+            <SummaryCard>
+                <header>
+                    <span>Entradas</span>
+                    <ArrowCircleUp size='32' color='#00B37E'/>
+                </header>
+
+                <strong>17.400,00</strong>
+            </SummaryCard>
+
+            <SummaryCard>
+                <header>
+                    <span>Saídas</span>
+                    <ArrowCircleDown size='32' color='#F75A68'/>
+                </header>
+
+                <strong>17.400,00</strong>
+            </SummaryCard>
+
+            <SummaryCard variant='green'>
+                <header>
+                    <span>Total</span>
+                    <CurrencyDollar size='32' color='#FFF'/>
+                </header>
+
+                <strong>17.400,00</strong>
+            </SummaryCard>
+        </SummaryContainer>
+    )
+}
