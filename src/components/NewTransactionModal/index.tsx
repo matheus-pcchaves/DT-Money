2605a6d643fc5cd246abce165e02678439996a32
Matheus <vicde2@hotmail.com>
2022-12-28 09:11:18 -0300

    DT-Money

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,28 +1,114 @@
-import * as Dialog from "@radix-ui/react-dialog";
-import { X } from "phosphor-react";
+import * as Dialog from '@radix-ui/react-dialog'
+import * as z from 'zod'
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
 
-import { CloseButton, Content, Overlay } from "./styles";
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from './styles'
+import { Controller, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useContext } from 'react'
+import { TransactionsContext } from '../../contexts/TransactionsContext'
 
-export function NewTransactionModal(){
-    return(
-        <Dialog.Portal>
-            <Overlay/>
+const newTransactionFormSchema = z.object({
+  description: z.string(),
+  price: z.number(),
+  category: z.string(),
+  type: z.enum(['income', 'outcome']),
+})
 
-            <Content>
-                <Dialog.Title>Nova transação</Dialog.Title>
+type NewtransactionFormInputs = z.infer<typeof newTransactionFormSchema>
 
-                <CloseButton>
-                    <X size={24}/>
-                </CloseButton>
+export function NewTransactionModal() {
+  const { createTransaction } = useContext(TransactionsContext)
 
-                <form action="">
-                    <input type="text" placeholder="Descrição" required/>
-                    <input type="number" placeholder="Preço" required/>
-                    <input type="text" placeholder="Categoria" required/>
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+    reset,
+  } = useForm<NewtransactionFormInputs>({
+    resolver: zodResolver(newTransactionFormSchema),
+  })
 
-                    <button type="submit">Cadastrar</button>
-                </form>
-            </Content>
-        </Dialog.Portal>
-    )
+  async function handleCreateNewTransaction(data: NewtransactionFormInputs) {
+    const { description, price, category, type } = data
+
+    await createTransaction({
+      description,
+      price,
+      category,
+      type,
+    })
+
+    reset()
+  }
+
+  return (
+    <Dialog.Portal>
+      <Overlay />
+
+      <Content>
+        <Dialog.Title>Nova transação</Dialog.Title>
+
+        <CloseButton>
+          <X size={24} />
+        </CloseButton>
+
+        <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
+          <input
+            type="text"
+            placeholder="Descrição"
+            required
+            {...register('description')}
+          />
+          <input
+            type="number"
+            placeholder="Preço"
+            required
+            {...register('price', { valueAsNumber: true })}
+          />
+          <input
+            type="text"
+            placeholder="Categoria"
+            required
+            {...register('category')}
+          />
+
+          <Controller
+            control={control}
+            name="type"
+            render={({ field }) => {
+              console.log(field)
+              return (
+                <TransactionType
+                  onValueChange={field.onChange}
+                  value={field.value}
+                >
+                  <TransactionTypeButton variant="income" value="income">
+                    <ArrowCircleUp size={24} />
+                    Entrada
+                  </TransactionTypeButton>
+
+                  <TransactionTypeButton variant="outcome" value="outcome">
+                    <ArrowCircleDown size={24} />
+                    Saída
+                  </TransactionTypeButton>
+                </TransactionType>
+              )
+            }}
+          />
+
+          <button type="submit" disabled={isSubmitting}>
+            Cadastrar
+          </button>
+        </form>
+      </Content>
+    </Dialog.Portal>
+  )
 }
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,6 @@
-import styled from "styled-components";
-import * as Dialog from "@radix-ui/react-dialog";
+import styled from 'styled-components'
+import * as Dialog from '@radix-ui/react-dialog'
+import * as RadioGroup from '@radix-ui/react-radio-group'
 
 export const Overlay = styled(Dialog.Overlay)`
   position: fixed;
@@ -7,13 +8,13 @@ export const Overlay = styled(Dialog.Overlay)`
   height: 100vh;
   inset: 0;
   background: rgba(0, 0, 0, 0.75);
-`;
+`
 
 export const Content = styled(Dialog.Content)`
   min-width: 32rem;
   border-radius: 6px;
-  padding: 2.5rem, 3rem;
-  background: ${props => props.theme['gray-800']};
+  padding: 2.5rem 3rem;
+  background: ${(props) => props.theme['gray-800']};
 
   position: fixed;
   top: 50%;
@@ -28,35 +29,35 @@ export const Content = styled(Dialog.Content)`
     gap: 1rem;
 
     input {
-        border-radius: 6px;
-        border: 0;
-        background: ${props => props.theme['gray-900']};
-        color: ${props => props.theme['gray-300']};
-        padding: 1rem;
+      border-radius: 6px;
+      border: 0;
+      background: ${(props) => props.theme['gray-900']};
+      color: ${(props) => props.theme['gray-300']};
+      padding: 1rem;
 
-        &::placeholder{
-            color: ${props => props.theme['gray-500']};
-        }
+      &::placeholder {
+        color: ${(props) => props.theme['gray-500']};
+      }
     }
 
-    button[type="submit"] {
-        height: 50px;
-        border: 0;
-        background: ${props => props.theme['green-500']};
-        color: ${props => props.theme.white};
-        font-weight: bold;
-        padding: 0 1.25rem;
-        border-radius: 6px;
-        margin-top: 1.5rem;
-        cursor: pointer;
+    button[type='submit'] {
+      height: 50px;
+      border: 0;
+      background: ${(props) => props.theme['green-500']};
+      color: ${(props) => props.theme.white};
+      font-weight: bold;
+      padding: 0 1.25rem;
+      border-radius: 6px;
+      margin-top: 1.5rem;
+      cursor: pointer;
 
-        &:hover {
-            background: ${props => props.theme['green-700']};
-            transition: background-color 0.2s;
-        }
-    }                    
+      &:hover {
+        background: ${(props) => props.theme['green-700']};
+        transition: background-color 0.2s;
+      }
+    }
   }
-`;
+`
 
 export const CloseButton = styled(Dialog.Close)`
   position: absolute;
@@ -66,18 +67,24 @@ export const CloseButton = styled(Dialog.Close)`
   right: 1.5rem;
   line-height: 0;
   cursor: pointer;
-  color: ${props => props.theme['gray-500']};
-`;
+  color: ${(props) => props.theme['gray-500']};
+`
 
-export const TransactionType = styled.div`
+export const TransactionType = styled(RadioGroup.Root)`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 1rem;
   margin-top: 0.5rem;
-`;
+`
 
-export const TransactionTypeButton = styled.button`
-  background: ${props => props.theme['gray-700']};
+interface TransactionTypeButtonProps {
+  variant: 'income' | 'outcome'
+}
+
+export const TransactionTypeButton = styled(
+  RadioGroup.Item,
+)<TransactionTypeButtonProps>`
+  background: ${(props) => props.theme['gray-700']};
   padding: 1rem;
   display: flex;
   align-items: center;
@@ -86,5 +93,24 @@ export const TransactionTypeButton = styled.button`
   border-radius: 6px;
   cursor: pointer;
   border: 0;
-  color: ${props => props.theme['gray-100']};
-`;
+  color: ${(props) => props.theme['gray-300']};
+
+  svg {
+    color: ${(props) =>
+      props.variant === 'income'
+        ? props.theme['green-300']
+        : props.theme['red-300']};
+  }
+
+  &[data-state='checked'] {
+    color: ${(props) => props.theme.white};
+    background: ${(props) =>
+      props.variant === 'income'
+        ? props.theme['green-500']
+        : props.theme['red-500']};
+
+    svg {
+      color: ${(props) => props.theme.white};
+    }
+  }
+`
